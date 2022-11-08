09190fd4b1c2015d96526ddc1f1def1d89dfd323
Seftianaaaa <114810868+Seftianaaaa@users.noreply.github.com>
2022-11-08 17:44:23 +0700

    Add files via upload

---
+++ paswa/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'db_paswa';
+$databaseUsername = 'root';
+$databasePassword = 'septi';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+?>
